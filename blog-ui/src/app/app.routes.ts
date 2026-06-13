cc376f2c1cc8f13fc8df9ca7c82a69cde14ff5d2
StepByStepCoding <mishradhan9876@gmail.com>
2026-06-13 01:44:07 -0400

    feat: Add navigation toolbar and home page (closes #8)

--- blog-ui/src/app/app.routes.ts
+++ blog-ui/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'posts/new', component: PostFormComponent },
   { path: 'posts/:id', component: PostDetailComponent },
-  { path: 'posts/:id/edit', component: PostFormComponent },
-  { path: '', redirectTo: '/posts', pathMatch: 'full' }
+  { path: 'posts/:id/edit', component: PostFormComponent }
 ];
