a04d57a79a9ab41ad468b7bd630fb7758377ffce
StepByStepCoding <mishradhan9876@gmail.com>
2026-06-13 01:38:42 -0400

    feat: Implement PostService methods (closes #4)

--- blog-ui/src/app/services/post.service.ts
+++ blog-ui/src/app/services/post.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 const API_URL = 'https://localhost:5001/api';
+
+export interface Post {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
   constructor(private http: HttpClient) {}
 
-  getPosts() {}
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${API_URL}/posts`);
+  }
 
-  getPost(id: number) {}
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>(`${API_URL}/posts/${id}`);
+  }
 
-  createPost(post: any) {}
+  createPost(post: Partial<Post>): Observable<Post> {
+    return this.http.post<Post>(`${API_URL}/posts`, post);
+  }
 
-  updatePost(id: number, post: any) {}
+  updatePost(id: number, post: Partial<Post>): Observable<Post> {
+    return this.http.put<Post>(`${API_URL}/posts/${id}`, { ...post, id });
+  }
 
-  deletePost(id: number) {}
+  deletePost(id: number): Observable<void> {
+    return this.http.delete<void>(`${API_URL}/posts/${id}`);
+  }
 }
